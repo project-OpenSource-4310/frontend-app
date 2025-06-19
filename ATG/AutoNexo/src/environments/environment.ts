6268c2e746f4a09233f47e3410317725bfd52d9a
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-06-19 16:03:56 -0500

    First commit Autonexo

---
+++ ATG/AutoNexo/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment={
+  production:true,
+  serverBaseUrl:'http://localhost:3000',
+  mechanicsEndpointPath:'/mechanics',
+  driversEndpointPath:'/drivers',
+  vehiclesEndpointPath:'/vehicles',
+  assignmentRequestsEndpointPath:'/assignment_requests',
+  maintenanceRequestsEndpointPath:'/maintenance_requests',
+  maintenancesEndpointPath:'/maintenances',
+  };
