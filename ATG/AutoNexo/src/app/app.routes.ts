e3895ba7eea583b9ad68f31663e51f0b76bb7cbb
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-06-19 17:24:50 -0500

    Add Register Component

--- ATG/AutoNexo/src/app/app.routes.ts
+++ ATG/AutoNexo/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {AuthenticationPageComponent} from './features/authentication/pages/authentication-page/authentication-page.component';
 const LoginComponent =()=>import("./features/authentication/components/login/login.component").then(m=>m.LoginComponent);
 const PageNotFoundComponent=()=>import('./shared/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
+const RegisterComponent =()=>import("./features/authentication/components/register/register.component").then(m=>m.RegisterComponent);
 export const routes: Routes = [
   { path: 'authentication',component:AuthenticationPageComponent,
     children: [
       { path: 'login', loadComponent: LoginComponent },
+      { path: 'register', loadComponent: RegisterComponent },
     ]},
   {path:'', redirectTo:'/authentication',pathMatch:'full'},
   {path:'**',loadComponent:PageNotFoundComponent},
