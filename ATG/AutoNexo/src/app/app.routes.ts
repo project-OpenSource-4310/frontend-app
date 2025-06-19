a2e8bcbe59de83daadf51bd4e8768668ae8a0d2d
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-06-19 17:30:07 -0500

    Add TermsAndCondition

--- ATG/AutoNexo/src/app/app.routes.ts
+++ ATG/AutoNexo/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-
+const TermsConditionsComponent =()=>import('./shared/pages/terms-conditions/terms-conditions.component').then(m=>m.TermsConditionsComponent);
 export const routes: Routes = [
-
+  {path:'terms-and-conditions',loadComponent:TermsConditionsComponent},
 ];
