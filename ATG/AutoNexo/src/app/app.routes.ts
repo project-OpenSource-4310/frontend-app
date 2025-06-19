f5c36d4f0a40a2329d37cfaccd3ef0612bc3648f
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-06-19 17:15:27 -0500

    Add Login Component

--- ATG/AutoNexo/src/app/app.routes.ts
+++ ATG/AutoNexo/src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
-
+import {AuthenticationPageComponent} from './features/authentication/pages/authentication-page/authentication-page.component';
+const LoginComponent =()=>import("./features/authentication/components/login/login.component").then(m=>m.LoginComponent);
+const PageNotFoundComponent=()=>import('./shared/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
 export const routes: Routes = [
-
+  { path: 'authentication',component:AuthenticationPageComponent,
+    children: [
+      { path: 'login', loadComponent: LoginComponent },
+    ]},
+  {path:'', redirectTo:'/authentication',pathMatch:'full'},
+  {path:'**',loadComponent:PageNotFoundComponent},
 ];
