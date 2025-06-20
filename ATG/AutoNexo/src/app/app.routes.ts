69c856d3eb9f9a354e318ded61308148a17bd411
RafaelVivanco <rafaelvivanco2@gmail.com>
2025-06-19 21:20:58 -0500

    Add mechanic-routes

--- ATG/AutoNexo/src/app/app.routes.ts
+++ ATG/AutoNexo/src/app/app.routes.ts
@@ -19,6 +19,17 @@ const MaintenanceRequestReceivedComponent=()=>import('./features/driver/componen
 const DriverRequestsReceivedComponent=()=>import('./features/driver/components/driver-requests-received/driver-requests-received.component').then(m=>m.DriverRequestsReceivedComponent);
 const DriverProfileComponent=()=>import('./features/driver/components/driver-profile/driver-profile.component').then(m=>m.DriverProfileComponent);
 
+// MECHANIC
+const MechanicPageComponent =()=>import('./features/mechanic/pages/mechanic-page/mechanic-page.component').then(m=>m.MechanicPageComponent);
+const MechanicHomeComponent=()=>import('./features/mechanic/components/mechanic-home/mechanic-home.component').then(m=>m.MechanicHomeComponent);
+const MechanicRequestsReceivedComponent=()=>import('./features/mechanic/components/mechanic-requests-received/mechanic-requests-received.component').then(m=>m.MechanicRequestsReceivedComponent);
+const MechanicRequestsSentComponent=()=>import('./features/mechanic/components/mechanic-requests-sent/mechanic-requests-sent.component').then(m=>m.MechanicRequestsSentComponent);
+const MechanicVehicleComponent=()=>import('./features/mechanic/components/mechanic-vehicle/mechanic-vehicle.component').then(m=>m.MechanicVehicleComponent);
+const AssignmentRequestReceivedComponent=()=>import('./features/mechanic/components/assignment-request-received/assignment-request-received.component').then(m=>m.AssignmentRequestReceivedComponent);
+const MaintenanceRequestSentComponent=()=>import('./features/mechanic/components/maintenance-request-sent/maintenance-request-sent.component').then(m=>m.MaintenanceRequestSentComponent);
+const SendMaintenanceRequestComponent=()=>import('./features/mechanic/components/send-maintenance-request/send-maintenance-request.component').then(m=>m.SendMaintenanceRequestComponent);
+const MechanicProfileComponent=()=>import('./features/mechanic/components/mechanic-profile/mechanic-profile.component').then(m=>m.MechanicProfileComponent);
+
 export const routes: Routes = [
   { path: 'authentication',component:AuthenticationPageComponent,
     children: [
@@ -38,7 +49,18 @@ export const routes: Routes = [
       {path:'requests-sent',loadComponent:DriverRequestsSentComponent},
       {path:'assignment-request',loadComponent:AssignmentRequestSentComponent},
       {path:'maintenance-request',loadComponent:MaintenanceRequestReceivedComponent},
+      {path:'profile',loadComponent:MechanicProfileComponent},
       {path:'profile',loadComponent:DriverProfileComponent},
+    ]},
+  {path:'mechanic-page',loadComponent:MechanicPageComponent,
+    children:[
+      {path:'home',loadComponent:MechanicHomeComponent},
+      {path:'requests-received',loadComponent:MechanicRequestsReceivedComponent},
+      {path:'assignment-request',loadComponent:AssignmentRequestReceivedComponent},
+      {path:'send-request',loadComponent:SendMaintenanceRequestComponent},
+      {path:'requests-sent',loadComponent:MechanicRequestsSentComponent},
+      {path:'maintenance-request',loadComponent:MaintenanceRequestSentComponent},
+      {path:'vehicle',loadComponent:MechanicVehicleComponent},
     ]},
   {path:'**',loadComponent:PageNotFoundComponent},
 ];
